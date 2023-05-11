7df8f2e869e1df1dba457d3f05c22388a57c5395
Marina <95050756+Marindala@users.noreply.github.com>
2023-05-11 02:51:20 -0300

    modified PropTypes and icons svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import './App.css' 
-import { Slides } from './components/Slides'
+import { Slides, Slide, TextoSlide } from './components/Slides'
 import styled from 'styled-components';
+import img1 from './assets/Slide1.jpg';
+import img2 from './assets/Slide2.jpg';
+import img3 from './assets/Slide3.jpg';
+import img4 from './assets/Slide4.jpg';
 
 const Titulo = styled.p`
 	font-size: 40px;
-	font-weight: 700;
+	font-weight: 900;
 	text-transform: uppercase;
 	margin-bottom: 10px;
 `;
@@ -16,10 +20,61 @@ function App() {
 
   return (
     <main>
-      <div>
-        <Titulo>Slides Code</Titulo>
-        <Slides/>
-       </div>
+      <Titulo>Experiencia Código</Titulo>
+			<Slides controles={true}>
+				<Slide>
+					<a href="https://portfolio-marina-marindala.netlify.app/#home">
+						<img src={img1} alt=""/>
+					</a>
+					<TextoSlide>
+						<p></p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://portfolio-marina-marindala.netlify.app/#home">
+						<img src={img2} alt=""/>
+					</a>
+					<TextoSlide>
+						<p></p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://portfolio-marina-marindala.netlify.app/#home">
+						<img src={img3} alt=""/>
+					</a>
+					<TextoSlide>
+						<p></p>
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://portfolio-marina-marindala.netlify.app/#home">
+						<img src={img4} alt=""/>
+					</a>
+					<TextoSlide>
+						<p>Experiencia Código</p>
+					</TextoSlide>
+				</Slide>
+			</Slides>
+
+			<Titulo>Mejora tú Código </Titulo>
+			<Slides controles={true} autoplay={true} velocidad="3000" intervalo="5000">
+				<Slide>
+					<a href="https://portfolio-marina-marindala.netlify.app/#home">
+						<img src={img1} alt=""/>
+					</a>
+					<TextoSlide >
+			
+					</TextoSlide>
+				</Slide>
+				<Slide>
+					<a href="https://portfolio-marina-marindala.netlify.app/#home">
+						<img src={img2} alt=""/>
+					</a>
+					<TextoSlide>
+						
+					</TextoSlide>
+				</Slide>
+			</Slides>
     </main>
   )
 }
--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -1,12 +1,211 @@
+import { useRef, useEffect, useCallback } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
+export const Slides = ({
+  children,
+  controles = false,
+  autoplay = false,
+  velocidad = "500",
+  intervalo = "5000",
+}) => {
+  const slideshow = useRef(null);
+  const intervaloSlideshow = useRef(null);
 
-export const Slides = () => {
+  const siguiente = useCallback(() => {
+    // Comprobamos que el slideshow tenga elementos
+    if (slideshow.current.children.length > 0) {
+      console.log("Siguiente");
+
+      // Obtenemos el primer elemento del slideshow.
+      const primerElemento = slideshow.current.children[0];
+
+      // Establecemos la transicion para el slideshow.
+      slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+
+      const tamañoSlide = slideshow.current.children[0].offsetWidth;
+
+      // Movemos el slideshow
+      slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+
+      const transicion = () => {
+        // Reiniciamos la posicion del Slideshow.
+        slideshow.current.style.transition = "none";
+        slideshow.current.style.transform = `translateX(0)`;
+
+        // Tomamos el primer elemento y lo mandamos al final.
+        slideshow.current.appendChild(primerElemento);
+
+        slideshow.current.removeEventListener("transitionend", transicion);
+      };
+
+      // Eventlistener para cuando termina la animacion.
+      slideshow.current.addEventListener("transitionend", transicion);
+    }
+  }, [velocidad]);
+
+  const anterior = () => {
+    console.log("Anterior");
+    if (slideshow.current.children.length > 0) {
+      // Obtenemos el ultimo elemento del slideshow.
+      const index = slideshow.current.children.length - 1;
+      const ultimoElemento = slideshow.current.children[index];
+      slideshow.current.insertBefore(
+        ultimoElemento,
+        slideshow.current.firstChild
+      );
+
+      slideshow.current.style.transition = "none";
+      const tamañoSlide = slideshow.current.children[0].offsetWidth;
+      slideshow.current.style.transform = `translateX(-${tamañoSlide}px)`;
+
+      setTimeout(() => {
+        slideshow.current.style.transition = `${velocidad}ms ease-out all`;
+        slideshow.current.style.transform = `translateX(0)`;
+      }, 30);
+    }
+  };
+
+  useEffect(() => {
+    if (autoplay) {
+      intervaloSlideshow.current = setInterval(() => {
+        siguiente();
+      }, intervalo);
+
+      // Eliminamos los intervalos
+      slideshow.current.addEventListener("mouseenter", () => {
+        clearInterval(intervaloSlideshow.current);
+      });
+
+      // Volvemos a poner el intervalo cuando saquen el cursor del slideshow
+      slideshow.current.addEventListener("mouseleave", () => {
+        intervaloSlideshow.current = setInterval(() => {
+          siguiente();
+        }, intervalo);
+      });
+    }
+  }, [autoplay, intervalo, siguiente]);
+
   return (
-    <div>
-      <div>
-        <a href="https://portfolio-marina-marindala.netlify.app/"></a>
-      </div>
-      <div>Slides</div>
-      </div>
-  )
-}
+    <ContenedorPrincipal>
+      <ContenedorSlideshow ref={slideshow}>{children}</ContenedorSlideshow>
+      {controles && (
+        <Controles>
+          <Boton onClick={anterior}>
+            <div>
+              <svg
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="evenodd"
+                clip="evenodd"
+              >
+                <path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z" />
+              </svg>
+            </div>
+          </Boton>
+          <Boton derecho onClick={siguiente}>
+            <>
+              {/*  {" "} */}
+              <svg
+                width="24"
+                height="24"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="evenodd"
+                clip="evenodd"
+              >
+                <path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z" />
+              </svg>
+            </>
+          </Boton>
+        </Controles>
+      )}
+    </ContenedorPrincipal>
+  );
+};
+
+const ContenedorPrincipal = styled.div`
+  position: relative;
+`;
+
+const ContenedorSlideshow = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+`;
+
+const Slide = styled.div`
+  min-width: 100%;
+  overflow: hidden;
+  transition: 0.3s ease all;
+  z-index: 10;
+  /* max-height: 500px; */
+  position: relative;
+
+  img {
+    width: 100%;
+    vertical-align: top;
+  }
+`;
+
+const TextoSlide = styled.div`
+  width: 100%;
+  padding: 10px 60px;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+
+  @media screen and (max-width: 700px) {
+    position: relative;
+    background: #000;
+  }
+`;
+
+const Controles = styled.div`
+  position: absolute;
+  top: 0;
+  z-index: 20;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+`;
+
+const Boton = styled.button`
+  pointer-events: all;
+  background: none;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  width: 50px;
+  height: 100%;
+  text-align: center;
+  position: absolute;
+  transition: 0.3s ease all;
+  /* &:hover {
+  background: rgba(0,0,0,.2);
+  path {
+    fill: #fff;
+  }
+} */
+
+  path {
+    filter: ${(props) =>
+      props.derecho
+        ? "drop-shadow(-2px 0px 0px #fff)"
+        : "drop-shadow(2px 0px 0px #fff)"};
+  }
+
+  ${(props) => (props.derecho ? "right: 0" : "left: 0")}
+`;
+
+Slides.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+  controles: PropTypes.bool,
+  autoplay: PropTypes.bool,
+  velocidad: PropTypes.string,
+  intervalo: PropTypes.string,
+};
+
+export { Slide, TextoSlide };
